f21e2600192f8d495c3ee82c746a2931127fc436
ibdj <ibdjacobsen@gmail.com>
2025-03-07 15:35:47 +0100

    added import of TWI calculate from arcticdem in gee

--- scripts/twi.R
+++ scripts/twi.R
@@ -5,8 +5,13 @@ dtm <- raster("data/Kobbefjord_2015_DTM_50cm.tif")
 
 slope <- terrain(dtm, opt = "slope", unit = "radians")
 flow_dir <- terrain(dtm, opt = "flowdir")
-flow_acc <- raster::area(flow_acc)
+flow_acc <- raster::area(flow_dir)
 
 twi <- log((flow_acc + 1) / (tan(slope) + 0.01))
 
+plot(twi)
+
+twi_arcticdem <- raster(file.choose())
+
+plot(twi_arcticdem)
 plot(twi)
